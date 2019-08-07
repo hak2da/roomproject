d77566d180d38efc068018d4cf46234f5298b2dc
Kim Hyun Tae <yunkeg03@naver.com>
2019-08-07 11:58:04 +0900

    a

---
+++ src/room/model/RoomVO.java
@@ -0,0 +1,120 @@
+package room.model;
+
+import java.util.Date;
+
+public class RoomVO {
+	private String address, image1,image2,image3,image4,image5,roomtype,mpay2,floor,title,content;
+	private int deposit,rent,mpay,rsize,parking,elve;
+	private Date rdate;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getImage1() {
+		return image1;
+	}
+	public void setImage1(String image1) {
+		this.image1 = image1;
+	}
+	public String getImage2() {
+		return image2;
+	}
+	public void setImage2(String image2) {
+		this.image2 = image2;
+	}
+	public String getImage3() {
+		return image3;
+	}
+	public void setImage3(String image3) {
+		this.image3 = image3;
+	}
+	public String getImage4() {
+		return image4;
+	}
+	public void setImage4(String image4) {
+		this.image4 = image4;
+	}
+	public String getImage5() {
+		return image5;
+	}
+	public void setImage5(String image5) {
+		this.image5 = image5;
+	}
+	public String getRoomtype() {
+		return roomtype;
+	}
+	public void setRoomtype(String roomtype) {
+		this.roomtype = roomtype;
+	}
+	public String getMpay2() {
+		return mpay2;
+	}
+	public void setMpay2(String mpay2) {
+		this.mpay2 = mpay2;
+	}
+	public String getFloor() {
+		return floor;
+	}
+	public void setFloor(String floor) {
+		this.floor = floor;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(int deposit) {
+		this.deposit = deposit;
+	}
+	public int getRent() {
+		return rent;
+	}
+	public void setRent(int rent) {
+		this.rent = rent;
+	}
+	public int getMpay() {
+		return mpay;
+	}
+	public void setMpay(int mpay) {
+		this.mpay = mpay;
+	}
+	public int getRsize() {
+		return rsize;
+	}
+	public void setRsize(int rsize) {
+		this.rsize = rsize;
+	}
+	public int getParking() {
+		return parking;
+	}
+	public void setParking(int parking) {
+		this.parking = parking;
+	}
+	public int getElve() {
+		return elve;
+	}
+	public void setElve(int elve) {
+		this.elve = elve;
+	}
+	public Date getRdate() {
+		return rdate;
+	}
+	public void setRdate(Date rdate) {
+		this.rdate = rdate;
+	}
+	
+	
+}
