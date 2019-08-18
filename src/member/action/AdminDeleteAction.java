817dbe73d748077b8c3b3fee9378a5120a9e750f
Seriously1 <stint@daum.net>
2019-08-18 17:39:10 +0900

    ddd
    1ck

---
+++ src/member/action/AdminDeleteAction.java
@@ -0,0 +1,36 @@
+package member.action;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.model.MemberDAO;
+
+public class AdminDeleteAction implements Action{
+	 public ActionForward execute(HttpServletRequest request,HttpServletResponse response) 
+			 	throws Exception{
+				 
+				ActionForward forward = new ActionForward();
+				request.setCharacterEncoding("UTF-8");
+				
+			   	boolean result=false;
+			   	boolean usercheck=false;
+			   	String id = request.getParameter("id");
+			   	
+			   	MemberDAO memberdao=new MemberDAO();
+			   	System.out.println("2222");
+			   	
+			   	
+			   	result=memberdao.memberDelete(id);
+			   	if(result==false){
+			   		System.out.println("회원 삭제 실패");
+			   		return null;
+			   	}
+			   	
+			   	System.out.println("회원 삭제 성공");
+			   	forward.setRedirect(true);
+		   		forward.setPath("./memberlist.to");
+		   		return forward;
+			 }
+}
